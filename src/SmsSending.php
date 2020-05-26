286fd1dfb537eed4f83fdd1a98675b64f1e35bfe
Chadli Ouhichi <louhichi.chadli93@outlook.fr>
2020-05-26 08:25:15 +0100

    Bundle  initialize

---
+++ src/SmsSending.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace DotIt\SmsSendingBundle;
+
+class SmsSending
+{
+
+}
---
+++ src/SmsSendingBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace DotIt\SmsSendingBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class SmsSendingBundle extends Bundle
+{
+
+}
